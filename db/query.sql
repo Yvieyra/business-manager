55f042e8e4e843e41a239ccf0020e5792ad4d633
Ying Yang <vieyrayessenia8@gmail.com>
2023-01-22 14:37:35 -0700

    added first question and answer successfully

---
+++ db/query.sql
@@ -0,0 +1,13 @@
+--view all departments (departments and department IDs)
+
+--view all roles (job title, role id, department that role belongs to, and salary)
+
+--view all employees (employee id, first name, last name, job title, departments, salaries, and managers)
+
+--add a department(department name)
+
+--add a role(name, salary, and department for the role)
+
+--add an employee(first name, last name, role, and manager)
+
+--Update an employee role(select employee, and update their new role)
--- index.js
+++ index.js
@@ -3,21 +3,36 @@ const fs = require('fs'); //Might not need this afterall
 const cTable = require('console.table'); //using console.table package 
 const mysql = require('mysql2'); // using mysql2 package
 
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password
+    password: 'Felizfamily2017!',
+    database: 'business_db'
+  },
+  console.log(`Connected to the business_db database.`)
+);
+
 
   const start = [
     {
         type: 'list',
         name: 'action',
         message: 'What would you like to do?',
-        choices: ['View all employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments, Add Department']
+        choices: ['View all employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View all departments', 'Add Department']
     },
 ]
     inquirer.prompt(start).then((answers) => {  
       console.log(answers.action);
       let userChoice = answers.action;
       //swith (if statements)
-      if (userChoice === 'View all employees') {
-        
+      if (userChoice === 'View all departments') {
+        db.query('SELECT * FROM department', function (err, results) {
+          console.table(results);
+        });
+      
       }
        (answers), (err) => 
             err ? console.log(err) : console.log('Success') 
