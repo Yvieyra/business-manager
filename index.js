ca1a66c558dfb72901ff19d3d8173540d7c7ecd2
Ying Yang <vieyrayessenia8@gmail.com>
2023-01-24 23:36:50 -0700

    department successfully adds to department table, need to resolve wrong table appearing

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const db = mysql.createConnection({
   database: "business_db",
 });
 
-
 //start of the program
 const question = [
   {
@@ -24,32 +23,36 @@ const question = [
       "Add Employee",
       "Update Employee Role",
       "View all roles",
-      "Add Role",
+      "Add role",
       "View all departments",
       "Add department",
     ],
   },
 ];
 function promptUser() {
-inquirer.prompt(question).then((answers) => {
-  console.log(answers.action);
-  let userChoice = answers.action;
-  //swith (if statements)
-  if (userChoice === "View all departments") {
-    viewDepartments();
-  }
+  inquirer.prompt(question).then((answers) => {
+    console.log(answers.action);
+    let userChoice = answers.action;
+    //swith (if statements)
+    if (userChoice === "View all departments") {
+      viewDepartments();
+    }
 
-  if (userChoice === "Add department") {
-    addDepartment();
-  }
+    if (userChoice === "Add department") {
+      addDepartment();
+    }
 
-  if (userChoice === 'View all roles') {
-    viewRoles();
-  }
-  (answers), (err) => 
-  err ? console.log(err) : console.log("Success");
-});
-};
+    if (userChoice === "View all roles") {
+      viewRoles();
+    }
+
+    if (userChoice === "Add role") {
+      addRole();
+    }
+
+    answers, (err) => (err ? console.log(err) : console.log("Success"));
+  });
+}
 
 promptUser();
 
@@ -61,28 +64,86 @@ const viewDepartments = () => {
 };
 
 const viewRoles = () => {
-  db.query("SELECT * FROM roles", function (err, results) {
+  db.query("SELECT * FROM roles", function (err, results) { //need to correct what is being selected and joined 
     console.table(results);
     promptUser();
   });
 };
 
-
-// Function to add a new department
 const addDepartment = () => {
-          newDepartment = [{
-            type: 'input',
-            name: 'dept',
-            message: 'What is the name of the department?',
-          }]
-        
-        inquirer.prompt(newDepartment).then((answers) => {
-          console.log(answers.dept);
-          db.query('INSERT INTO department (department_name) VALUES(?)', ('department_name'), function (err, results) {
-            console.table(results);
-        });
-          (answers), (err) => 
-            err ? console.log(err) : console.log('Success') 
-            promptUser();
-        });
-      };
+  newDepartment = [
+    {
+      type: "input",
+      name: "dept",
+      message: "What is the name of the department?",
+    },
+  ];
+  inquirer.prompt(newDepartment).then((answers) => {
+    const sql = `INSERT INTO department (department_name) VALUES (?)`;
+    const params = [answers.dept];
+
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        console.log("ERROR");
+      } else {
+        console.table(result); //the incorrect table is displaying 
+      }
+      answers, (err) => (err ? console.log(err) : console.log("Success"));
+      promptUser();
+    });
+  });
+};
+
+const addRole = () => {
+  newRole = [
+    {
+      type: "input",
+      name: "role",
+      message: "What is the new role?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "What is the salary?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "What department does this role belong to?",
+    },
+  ];
+  inquirer.prompt(newRole).then((answers) => {
+    const sql = `INSERT INTO role (title, salary, department) VALUES (?)`;
+    const params = [answers.role, ];
+
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        console.log("ERROR");
+      } else {
+        console.table(result);
+      }
+      answers, (err) => (err ? console.log(err) : console.log("Success"));
+      promptUser();
+    });
+  });
+};
+
+//Function to add a new department
+// const addDepartment = () => {
+//           newDepartment = [{
+//             type: 'input',
+//             name: 'dept',
+//             message: 'What is the name of the department?',
+//           }]
+
+//         inquirer.prompt(newDepartment).then((answers) => {
+//           console.log(answers.dept);
+//          const params = [answers.dept]
+//           db.query('INSERT INTO department (department_name) VALUES(?)', params, function (err, results) {
+//             console.table(results);
+//         });
+//           (answers), (err) =>
+//             err ? console.log(err) : console.log('Success')
+//             promptUser();
+//         });
+//       };
